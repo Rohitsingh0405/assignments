ec4f6132db72931d2437a61cc77ecc053e2ff8fa
Rohit Singh <rs21Rohit@gmail.com>
2025-03-04 10:40:44 +0530

    ratelimitter is used to count to number of user per second does not exceed

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,5 +1,4 @@
-const request = require('supertest');
-const assert = require('assert');
+
 const express = require('express');
 const app = express();
 // You have been given an express server which has a few endpoints.
@@ -16,6 +15,23 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function(req,res,next){
+  const userId= req.headers["user-Id"];
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId]+1;
+    if(numberOfRequestsForUser[userId]>5){
+      res.status(404).send("No entry for you ")
+    }
+    else{
+      next()
+    }
+
+  }
+  else{
+    numberOfRequestsForUser[userId]=1
+    next()
+  }
+})
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
